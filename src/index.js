ec451fa821914142d9dbc746613e1441241c21fe
Sheldon.li <765985232@qq.com>
2024-06-08 18:55:40 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -4,13 +4,13 @@ addEventListener("fetch", (event) => {
 });
 
 const routes = {
-  "docker.libcuda.so": "https://registry-1.docker.io",
-  "quay.libcuda.so": "https://quay.io",
-  "gcr.libcuda.so": "https://gcr.io",
-  "k8s-gcr.libcuda.so": "https://k8s.gcr.io",
-  "k8s.libcuda.so": "https://registry.k8s.io",
-  "ghcr.libcuda.so": "https://ghcr.io",
-  "cloudsmith.libcuda.so": "https://docker.cloudsmith.io",
+  "docker.1111119.xyz": "https://registry-1.docker.io",
+  "quay.1111119.xyz": "https://quay.io",
+  "gcr.1111119.xyz": "https://gcr.io",
+  "k8s-gcr.1111119.xyz": "https://k8s.gcr.io",
+  "k8s.1111119.xyz": "https://registry.k8s.io",
+  "ghcr.1111119.xyz": "https://ghcr.io",
+  "cloudsmith.1111119.xyz": "https://docker.cloudsmith.io",
 };
 
 function routeByHosts(host) {
